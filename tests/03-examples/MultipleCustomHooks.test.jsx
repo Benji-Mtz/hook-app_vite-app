a246272c7958cd3a58263c8cc32ae7521c6f65aa
Benji Martinez <benjim.flores@outlook.com>
2022-08-15 11:28:08 -0500

    test primer mock

---
+++ tests/03-examples/MultipleCustomHooks.test.jsx
@@ -0,0 +1,69 @@
+const { render, screen, fireEvent } = require("@testing-library/react");
+const { MultipleCustomHooks } = require("../../src/03-examples/MultipleCustomHooks");
+const { useCounter } = require("../../src/hooks/useCounter");
+const { useFetch } = require("../../src/hooks/useFetch");
+
+jest.mock("../../src/hooks/useFetch");
+jest.mock("../../src/hooks/useCounter");
+
+describe('Pruebas en <MultipleCustomHooks />', () => { 
+
+    const mockIncrement = jest.fn();
+        
+    useCounter.mockReturnValue({
+        counter: 1,
+        increment: mockIncrement
+    });
+
+    beforeEach( ()=> {
+        jest.clearAllMocks();
+    })
+
+    test("debe mostrar el componente por defecto", () => {
+        
+        useFetch.mockReturnValue({
+            data: null,
+            isLoading: true,
+            hasError: null
+        })
+
+        render( <MultipleCustomHooks />)
+
+        expect( screen.getByText('Loading...') );
+        expect( screen.getByText('BreakingBad Quotes') );
+
+        const nextButton = screen.getByRole('button', { name: 'Next quote' });
+        expect( nextButton.disabled ).toBeTruthy();
+        // screen.debug();
+    });
+    test("debe de mostrar un quote", () => {
+
+        useFetch.mockReturnValue({
+            data: [{ author: 'Benji', quote: 'Hola Mundo!'}],
+            isLoading: false,
+            hasError: null
+        })
+
+        render( <MultipleCustomHooks />)
+        
+        expect( screen.getByText('Hola Mundo!') ).toBeTruthy();
+        expect( screen.getByText('Benji') ).toBeTruthy();
+
+        const nextButton = screen.getByRole('button', { name: 'Next quote' });
+        expect( nextButton.disabled ).toBeFalsy();
+    });
+
+    test("debe de llamar la funcion de incrementar", () => {
+        useFetch.mockReturnValue({
+            data: [{ author: 'Benji', quote: 'Hola Mundo!'}],
+            isLoading: false,
+            hasError: null
+        });
+
+        render( <MultipleCustomHooks />);
+        const nextButton = screen.getByRole('button', { name: 'Next quote' });
+        fireEvent.click( nextButton );
+
+        expect( mockIncrement ).toHaveBeenCalled();
+    });
+});
